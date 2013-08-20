e88cdb11a51f01a9f4242af027b64f90f6dd429e
Jussi Vaihia <jussi.vaihia@futurice.com>
2013-08-20 14:47:55 +0300

    support modify (0,'key', None)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='mockldap',
-    version='0.1.0',
+    version='0.1.2futu',
     description=u"A simple mock implementation of python-ldap.",
     long_description=open('README').read(),
     url='http://bitbucket.org/psagers/mockldap/',
