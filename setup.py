8639b4b67eefd60f981670efd85b0b116e128f8d
Jussi Vaihia <jussi.vaihia@futurice.com>
2015-01-22 13:29:34 +0200

    -fork

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='mockldap-fork',
-    version='0.1.4',
+    version='0.1.5',
     description=u"Mockldap fork, A simple mock implementation of python-ldap.",
     long_description=open('README').read(),
     url='http://bitbucket.org/psagers/mockldap/',
